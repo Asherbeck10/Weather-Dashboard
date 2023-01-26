e7bd2eab93ffc74a497ed15235cf0d10d44d274a
Asherbeck10 <asher.bec@gmail.com>
2023-01-26 11:57:54 +0000

    getItem added

--- starter/script.js
+++ starter/script.js
@@ -5,13 +5,23 @@ let cities=[]
 $("#search-button").on("click", function(event) {
   event.preventDefault();
   let city=$("#search-input").val().trim();
+  if (city==="") {
+    city="London"
+    
+  }
   console.log(city);
   cities.push(city);
   console.log(cities)
   localStorage.setItem("cities",JSON.stringify(cities))
-//   let movieBtn=document.createElement("button")
+});
+if (!JSON.parse(localStorage.getItem("cities")) ) {
+  console.log("empty")
+  
+}else{cities=JSON.parse(localStorage.getItem("cities")) 
+console.log(cities)}
+
+
+
+ //   let movieBtn=document.createElement("button")
 //   movieBtn.textContent=movie
 //   buttonsViewEl.append(movieBtn)
-   
-
- });
