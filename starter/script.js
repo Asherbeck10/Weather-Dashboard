3266990b069ea6e7fa9b74014f1a8fb94a31c3e8
Asherbeck10 <asher.bec@gmail.com>
2023-01-26 19:04:29 +0000

    fetch function added

--- starter/script.js
+++ starter/script.js
@@ -1,27 +1,68 @@
-let cities=[]
 
-//city search
+let listGroupEl=document.querySelector("#history");
+let APIKey = "21fb092a8c84c334822d90684ec401e3";
+let queryCity ="";
+
+
+
+
+
+//retrieving info
+let cities=[]
+if (!JSON.parse(localStorage.getItem("cities")) ) {
+  console.log("empty")
+  
+}else{cities=JSON.parse(localStorage.getItem("cities")) 
+
+for (let i = 0; i < cities.length; i++) {
+  const btnText = cities[i];
+  let oldBtn=document.createElement("button")
+  oldBtn.textContent=btnText
+  listGroupEl.appendChild(oldBtn)
+
+  
+}}
+
+//city search form 
 
 $("#search-button").on("click", function(event) {
   event.preventDefault();
   let city=$("#search-input").val().trim();
   if (city==="") {
-    city="London"
+    city="London";
     
-  }
-  console.log(city);
-  cities.push(city);
-  console.log(cities)
-  localStorage.setItem("cities",JSON.stringify(cities))
-});
-if (!JSON.parse(localStorage.getItem("cities")) ) {
-  console.log("empty")
+  };
   
-}else{cities=JSON.parse(localStorage.getItem("cities")) 
-console.log(cities)}
+  //and new button and appending city list
+  cities.push(city);
+  queryCity=city
+  localStorage.setItem("cities",JSON.stringify(cities));
+  let newBtn=document.createElement("button");
+  newBtn.textContent=city;
+  listGroupEl.appendChild(newBtn);
+  weatherInfo(queryCity);
+  
+  
+});
+  
 
 
 
- //   let movieBtn=document.createElement("button")
-//   movieBtn.textContent=movie
-//   buttonsViewEl.append(movieBtn)
+//retrieving info from OpenWeather
+
+function weatherInfo(queryCity) {
+  let queryURL = "https://api.openweathermap.org/data/2.5/weather?&appid=21fb092a8c84c334822d90684ec401e3&q="+queryCity;
+  fetch(queryURL)
+.then(response => response.json())
+.then(function(weather) {
+  console.log(weather.name);
+  console.log(weather.dt);
+  console.log(weather.weather[0].icon);
+  console.log((weather.main.temp)-273.15);
+  console.log(weather.main.humidity);
+  console.log(weather.wind.speed);
+  
+  }) 
+  
+}
+ 
