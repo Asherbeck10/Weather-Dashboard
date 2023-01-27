9a62750bff7473c88cdf57f7c7bc06a674bc3e09
Asherbeck10 <asher.bec@gmail.com>
2023-01-27 10:49:37 +0000

    soft HTML for new cities

--- starter/script.js
+++ starter/script.js
@@ -1,8 +1,8 @@
 
 let listGroupEl=document.querySelector("#history");
 let queryCity ="London";
-let queryURL = "https://api.openweathermap.org/data/2.5/weather?&appid=21fb092a8c84c334822d90684ec401e3&q=";
-let sectionEl=document.querySelector("section")
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=21fb092a8c84c334822d90684ec401e3&q=";
+let todayEl=document.querySelector("#today")
 weatherInfo(queryCity)
 
 
@@ -17,6 +17,7 @@ if (!JSON.parse(localStorage.getItem("cities")) ) {
 for (let i = 0; i < cities.length; i++) {
   const btnText = cities[i];
   let oldBtn=document.createElement("button")
+  oldBtn.setAttribute("name","city-btn")
   oldBtn.textContent=btnText
   listGroupEl.appendChild(oldBtn)
 
@@ -55,14 +56,32 @@ function weatherInfo(queryCity) {
   fetch(NewQueryURL)
 .then(response => response.json())
 .then(function(weather) {
+  console.log(weather)
   console.log(weather.name);
   console.log(weather.dt);
   console.log(weather.weather[0].icon);
-  console.log((weather.main.temp)-273.15);
+  console.log((weather.main.temp));
   console.log(weather.main.humidity);
   console.log(weather.wind.speed);
+  let cityDate=moment((weather.dt)*1000).format('DD/MM/YYYY'); 
+  console.log(cityDate)
+  todayEl.innerHTML=` <h3>${weather.name}</h3>
+  <h3>(${cityDate})</h3>
+  <img src="http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" alt="weather-icon">
+  <div>
+    <div>Temp:</div>
+    <div>${weather.main.temp} C</div>
+  </div>
+  <div>
+    <div>Wind:</div>
+    <div>${weather.wind.speed}KPH</div>
+  </div>
+  <div>
+    <div>Humidity:</div>
+    <div>${weather.main.humidity}%</div>
+  </div>`
+  
   
   }) 
   
 }
- 
