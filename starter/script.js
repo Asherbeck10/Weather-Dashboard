198c464bb2b6748271b62955dad5967257a8b311
Asherbeck10 <asher.bec@gmail.com>
2023-01-27 18:37:07 +0000

    HTML el created

--- starter/script.js
+++ starter/script.js
@@ -1,8 +1,10 @@
 
-let listGroupEl=document.querySelector("#history");
-let queryCity ="London";
+let listGroupEl = document.querySelector("#history");
+let queryCity = "London";
 let queryURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=21fb092a8c84c334822d90684ec401e3&q=";
-let todayEl=document.querySelector("#today");
+let todayEl = document.querySelector("#today");
+let forecastEl = document.querySelector("#forecast");
+
 
 weatherInfo(queryCity);
 forecastQuery(queryCity);
@@ -10,69 +12,71 @@ forecastQuery(queryCity);
 
 
 //retrieving info
-let cities=[]
-if (!JSON.parse(localStorage.getItem("cities")) ) {
+let cities = []
+if (!JSON.parse(localStorage.getItem("cities"))) {
   console.log("empty")
-  
-}else{cities=JSON.parse(localStorage.getItem("cities")) 
 
-for (let i = 0; i < cities.length; i++) {
-  const btnText = cities[i];
-  let oldBtn=document.createElement("button")
-  oldBtn.classList.add("city-btn")
-  oldBtn.textContent=btnText
-  listGroupEl.appendChild(oldBtn);
+} else {
+  cities = JSON.parse(localStorage.getItem("cities"))
 
-  
-}}
+  for (let i = 0; i < cities.length; i++) {
+    const btnText = cities[i];
+    let oldBtn = document.createElement("button")
+    oldBtn.classList.add("city-btn")
+    oldBtn.textContent = btnText
+    listGroupEl.appendChild(oldBtn);
+
+
+  }
+}
 
 //city search form 
 
-$("#search-button").on("click", function(event) {
+$("#search-button").on("click", function (event) {
   event.preventDefault();
-  let city=$("#search-input").val().trim();
-  if (city==="") {
-    city="London";
-    
+  let city = $("#search-input").val().trim();
+  if (city === "") {
+    city = "London";
+
   };
-  
+
   //and new button and appending city list
   cities.push(city);
-  queryCity=city
-  localStorage.setItem("cities",JSON.stringify(cities));
-  let newBtn=document.createElement("button");
-  newBtn.textContent=city;
+  queryCity = city
+  localStorage.setItem("cities", JSON.stringify(cities));
+  let newBtn = document.createElement("button");
+  newBtn.textContent = city;
   listGroupEl.appendChild(newBtn);
   weatherInfo(queryCity);
-  
-  
-  
+
+
+
 });
-  
+
 
 
 
 //reading info for cities btn
-  
-$(document).on("click",".city-btn",function(event) {
+
+$(document).on("click", ".city-btn", function (event) {
   event.preventDefault();
-  queryCity=event.target.innerHTML;
+  queryCity = event.target.innerHTML;
   weatherInfo(queryCity)
   forecastQuery(queryCity);
-  
+
 })
 //function :retrieving info from OpenWeather for current weather
 
 function weatherInfo(queryCity) {
-  let NewQueryURL=queryURL+queryCity;
-   fetch(NewQueryURL)
- .then(response => response.json())
- .then(function(weather) {
-  //  console.log(weather)
-  
-   let cityDate=moment((weather.dt)*1000).format('DD/MM/YYYY'); 
-  //  console.log(cityDate)
-   todayEl.innerHTML=` <h3>${weather.name}</h3>
+  let NewQueryURL = queryURL + queryCity;
+  fetch(NewQueryURL)
+    .then(response => response.json())
+    .then(function (weather) {
+      //  console.log(weather)
+
+      let cityDate = moment((weather.dt) * 1000).format('DD/MM/YYYY');
+      //  console.log(cityDate)
+      todayEl.innerHTML = ` <h3>${weather.name}</h3>
    <h3>(${cityDate})</h3>
    <img src="http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" alt="weather-icon">
    <div>
@@ -87,42 +91,74 @@ function weatherInfo(queryCity) {
      <div>Humidity:</div>
      <div>${weather.main.humidity}%</div>
    </div>`;
-   
-   
-   }) 
- }
 
- // 5 days forecast
- function forecastQuery(queryCity) {
+
+    })
+}
+
+// 5 days forecast function
+function forecastQuery(queryCity) {
   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${queryCity}&limit=1&appid=21fb092a8c84c334822d90684ec401e3`)
-      .then(response => response.json())
-      .then(city=>{
-        console.log(city)
-       
-       return   fetch(`https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${city[0].lat}&lon=${city[0].lon}&appid=21fb092a8c84c334822d90684ec401e3`)
-      } )
+    .then(response => response.json())
+    .then(city => {
+      console.log(city)
 
-     .then(response =>response.json())
-      .then(weatherForecast =>{
-        console.log(weatherForecast)
-        console.log(weatherForecast.city.name)
-        for (let i = 7; i <(weatherForecast.list).length; i=i+8) {
-          let forecastDate=moment((weatherForecast.list[i].dt)*1000).format('DD/MM/YYYY');
-          console.log("Temp:"+ weatherForecast.list[i].main.temp)
-          console.log("Humidity:"+ weatherForecast.list[i].main.humidity)
-          console.log("icon:"+ weatherForecast.list[i].weather[0].icon)
+      return fetch(`https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${city[0].lat}&lon=${city[0].lon}&appid=21fb092a8c84c334822d90684ec401e3`)
+    })
 
-          console.log("Wind:" + weatherForecast.list[i].wind.speed)
-          console.log(forecastDate);
-           
-         
-        }
+    .then(response => response.json())
+    .then(weatherForecast => {
+      console.log(weatherForecast)
+      console.log(weatherForecast.city.name)
+      let forecastDates=[]
+      let icons=[]
+      //creating forecast cards
+      for (let i = 7; i < (weatherForecast.list).length; i = i + 8) {
+        let forecastDate = moment((weatherForecast.list[i].dt) * 1000).format('DD/MM/YYYY');
+        forecastDates.push(forecastDate)
+        icons.push(weatherForecast.list[i].weather[0].icon)
+        console.log("Temp:" + weatherForecast.list[i].main.temp)
+
+        console.log("Humidity:" + weatherForecast.list[i].main.humidity)
        
         
+        
+
+        console.log("Wind:" + weatherForecast.list[i].wind.speed)
+        console.log(forecastDate);
        
-      })
-  
- }
- 
+
+      
+      
+    }
+    console.log(icons)
+    forecastEl.innerHTML = ` <div class="card" style="width: 10rem;">
+      <div class="card-body">
+      <h5 class="card-title">${forecastDates[0]}</h5>
+      <img src="http://openweathermap.org/img/wn/${icons[0]}@2x.png" alt="weather-icon">
+      <div class="card-text">Temp:20 C</div>
+      <div class="card-text">Wind: 30 KPH</div>
+      <div class="card-text">Humidity: 30%</div>
+      
+       </div>
+      </div>
+    
+       <div class="card" style="width: 10rem;">
+      <div class="card-body">
+      <h5 class="card-title">${forecastDates[1]}</h5>
+      <img src="http://openweathermap.org/img/wn/${icons[1]}@2x.png" alt="weather-icon">
+      <div class="card-text">Temp:20 C</div>
+      <div class="card-text">Wind: 30 KPH</div>
+      <div class="card-text">Humidity: 30%</div>
+      
+       </div>
+      </div>`
+
+
+
+    })
+
+}
+
 
 
